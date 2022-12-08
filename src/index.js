d9eca874daad76c94ffcfa6c4178ef4d6a2f128f
diego <dgcuenca@gmail.com>
2022-12-08 15:57:13 -0500

    Reasign index after delete a task

--- src/index.js
+++ src/index.js
@@ -1,21 +1,90 @@
-import _ from 'lodash';
+import _, { isLength } from 'lodash';
 import './style.css';
-
-const toDoTasks = [
-  {
-    description: 'index10',
-    completed: true,
-    index: 10,
-  },
-];
-
-const orderToDoTasks = toDoTasks.sort((a, b) => (a.index > b.index ? 1 : -1));
-
+let description;
+let completed;
+let index;
+let bookList = JSON.parse(localStorage.getItem('bookList')) || [];
 let bookgenerator = '';
-const listaUl = document.getElementById('list-ul');
+const taskList = document.getElementById('taskList');
+let bookListDeleted=[];
 
-for (let i = 0; i < orderToDoTasks.length; i += 1) {
-  bookgenerator += `<li >"${orderToDoTasks[i].description}"</li>`;
+class Book {
+  constructor(description, completed, index) {
+    this.description = description;
+    this.completed = completed;
+    this.index = index;
+  }
 }
 
-listaUl.innerHTML = bookgenerator;
+function getTask() {
+  description = input.value;
+  completed = false;
+  index = bookList.length + 1;
+}
+
+function addBook(description, completed, index) {
+  const newBook = new Book(description, completed, index);
+  bookList.push(newBook);
+  localStorage.setItem('bookList', JSON.stringify(bookList));
+}
+
+function display(bookList) {
+  bookgenerator = '';
+  for (let i = 0; i < bookList.length; i += 1) {
+    bookgenerator += `<div>
+    <input type="text" id="${bookList[i].index * 10}" placeholder="${bookList[i].description}">
+    <button type="button" id="${bookList[i].index}">Remove</button></div>`;
+  }
+  taskList.innerHTML = bookgenerator;
+}
+
+function remove(event) {
+  const remove = event.target.closest('BUTTON');
+  if (remove) {
+    const buttonID = Number(event.target.id);
+    bookList = bookList.filter((book) => book.index !== buttonID);
+    bookListDeleted=[];
+    for (let i = 0; i < bookList.length; i += 1) {
+      const newBook = new Book(bookList[i].description, bookList[i].completed, i+1);
+      bookListDeleted.push(newBook);
+    }
+    bookList = bookListDeleted;
+    console.log(bookList);
+    localStorage.setItem('bookList', JSON.stringify(bookList));
+    display(bookList);
+  }
+}
+
+const input = document.getElementById("myInput");
+input.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    getTask();
+    addBook(description, completed, index);
+    display(bookList);
+  }
+});
+
+const btnAdd = document.getElementById('btnAdd');
+btnAdd.addEventListener('click', () => {
+  getTask();
+  addBook(description, completed, index);
+  display(bookList);
+})
+
+taskList.addEventListener('click', remove);
+
+
+taskList.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    const edit = event.target.closest('INPUT');
+    const editClass = Number(event.target.id) / 10;
+    let editTask = edit.value;
+    edit.value = editTask;
+    bookList[editClass - 1].description = editTask;
+    localStorage.setItem('bookList', JSON.stringify(bookList));
+  }
+});
+
+display(bookList);
+
+
