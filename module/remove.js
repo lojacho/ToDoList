c788f6e5616dc1570660d0a30e5759da79554173
diego <dgcuenca@gmail.com>
2022-12-14 19:47:05 -0500

    Improve ES06 modules aplication

--- module/remove.js
+++ module/remove.js
@@ -1,18 +1,14 @@
-/* eslint-disable import/no-cycle */
-/* eslint-disable import/no-mutable-exports */
-
 import { bookList, Book } from '../src/index.js';
 
 let bookListDeleted = [];
 let bookListFiltered = [];
 
-function remove(ID) {
+export default function remove(ID) {
   bookListFiltered = bookList.filter((book) => book.index !== ID);
   bookListDeleted = [];
   for (let i = 0; i < bookListFiltered.length; i += 1) {
     const newBook = new Book(bookListFiltered[i].description, bookListFiltered[i].completed, i + 1);
     bookListDeleted.push(newBook);
   }
+  return bookListDeleted;
 }
-
-export { remove, bookListDeleted };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-/* eslint-disable import/no-cycle */
-/* eslint-disable import/no-mutable-exports */
 // import _, { isLength } from 'lodash';
 
 import './style.css';
 import Book from '../module/bookClass.js';
-import { remove, bookListDeleted } from '../module/remove.js';
-import { clearAllTask, notCompleted } from '../module/checkbox.js';
+import remove from '../module/remove.js';
+import clearAllTask from '../module/checkbox.js';
 
 let description;
 let completed;
@@ -14,6 +12,8 @@ let bookList = JSON.parse(localStorage.getItem('bookList')) || [];
 let bookgenerator = '';
 const taskList = document.getElementById('taskList');
 const input = document.getElementById('myInput');
+const btnAdd = document.getElementById('btnAdd');
+const clearAll = document.querySelector('.footer');
 
 function getTask() {
   description = input.value;
@@ -38,21 +38,6 @@ function display(bookList) {
   taskList.innerHTML = bookgenerator;
 }
 
-input.addEventListener('keypress', (event) => {
-  if (event.key === 'Enter') {
-    getTask();
-    addBook(description, completed, index);
-    display(bookList);
-  }
-});
-
-const btnAdd = document.getElementById('btnAdd');
-btnAdd.addEventListener('click', () => {
-  getTask();
-  addBook(description, completed, index);
-  display(bookList);
-});
-
 function burbujeo(event) {
   const removeBtn = event.target.closest('BUTTON');
   let buttonID;
@@ -64,12 +49,29 @@ function burbujeo(event) {
   return buttonID;
 }
 
+// Checking if a task need to be added 
+
+input.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
+    getTask();
+    addBook(description, completed, index);
+    display(bookList);
+  }
+});
+
+btnAdd.addEventListener('click', (event) => {
+  event.stopPropagation();
+  getTask();
+  addBook(description, completed, index);
+  display(bookList);
+});
+
+// checking if a task has to be removed
+
 taskList.addEventListener('click', (event) => {
-  // event.stopPropagation();
   const ID = burbujeo(event);
   if (ID) {
-    remove(ID);
-    bookList = bookListDeleted;
+    bookList = remove(ID);
     localStorage.setItem('bookList', JSON.stringify(bookList));
     display(bookList);
   }
@@ -102,11 +104,8 @@ taskList.addEventListener('click', (event) => {
   }
 });
 
-const clearAll = document.querySelector('.footer');
-
 clearAll.addEventListener('click', () => {
-  clearAllTask();
-  bookList = notCompleted;
+  bookList = clearAllTask();
   localStorage.setItem('bookList', JSON.stringify(bookList));
   display(bookList);
 });
